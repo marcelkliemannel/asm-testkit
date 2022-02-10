7aa2e3985e2607c39abb63ec49dd1395113fa532
Marcel Kliemannel <dev@marcelkliemannel.com>
2022-02-10 20:23:39 +0100

    Add one by one comparison to AsmIterableAssert

--- src/main/java/dev/turingcomplete/asmtestkit/assertion/AsmIterableAssert.java
+++ src/main/java/dev/turingcomplete/asmtestkit/assertion/AsmIterableAssert.java
@@ -2,13 +2,18 @@ package dev.turingcomplete.asmtestkit.assertion;
 
 import dev.turingcomplete.asmtestkit.assertion._internal.AsmWritableAssertionInfo;
 import dev.turingcomplete.asmtestkit.comparator._internal.WithLabelNamesAsmComparatorAdapter;
+import dev.turingcomplete.asmtestkit.representation.AsmRepresentation;
 import org.assertj.core.api.AbstractIterableAssert;
 import org.assertj.core.api.Assertions;
-import org.assertj.core.internal.Failures;
+import org.assertj.core.presentation.Representation;
+import org.assertj.core.util.IterableUtil;
 
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * The base class for all ASM {@link Iterable} related assertions.
@@ -23,9 +28,11 @@ public class AsmIterableAssert<S extends AsmIterableAssert<S, E, A>, E, A extend
   // -- Class Fields ------------------------------------------------------------------------------------------------ //
   // -- Instance Fields --------------------------------------------------------------------------------------------- //
 
-  private final Failures failures = Failures.instance();
-
   private final Function<E, A> createElementAssert;
+
+  private Function<E, String> compareOneByOneKeyExtractor = __ -> {
+    throw new UnsupportedOperationException("No key extractor for one by one comparison set.");
+  };
 
   // -- Initialization ---------------------------------------------------------------------------------------------- //
 
@@ -110,6 +117,8 @@ public class AsmIterableAssert<S extends AsmIterableAssert<S, E, A>, E, A extend
    * <p>In the case {@code expected == null}, the super methods throws a generic
    * {@link NullPointerException}, without pointing out in detail that causes the
    * error. We want to fix that here with a more detailed error message.
+   *
+   * @return {@code this} {@link S}; never null.
    */
   @Override
   protected S containsExactlyInAnyOrderForProxy(E[] expected) {
@@ -119,15 +128,115 @@ public class AsmIterableAssert<S extends AsmIterableAssert<S, E, A>, E, A extend
     }
 
     if (actual != null) {
-      Assertions.assertThat(expected)
-                .as(descriptionText())
-                .withFailMessage(String.format("%nExpecting expected value not to be null if actual value is not null"))
-                .isNotNull();
+      assertExpectedNotNullOfActualIsNotNull(expected);
     }
+    assert actual != null && expected != null;
 
     return super.containsExactlyInAnyOrderForProxy(expected);
   }
 
+  /**
+   * Compares the given {@link Iterable} of {@link E}s one by one.
+   *
+   * <p>The AssertJ method {@link #containsExactlyInAnyOrderElementsOf} will
+   * output a full list of all non-matching elements which is very difficult to
+   * read if the textual representation of the elements spans several lines.
+   * This method first searches for a matching actual element (using the search
+   * key from {@link #setCompareOneByOneKeyExtractor(Function)}) and then just
+   * compares this one element.
+   *
+   * @param expected an {@link Iterable} of {@link E}s; may be null.
+   * @return {@code this} {@link S}; never null.
+   * @see #setCompareOneByOneKeyExtractor(Function)
+   */
+  public S containsExactlyInAnyOrderCompareOneByOneElementsOf(Iterable<E> expected) {
+    return containsExactlyInAnyOrderCompareOneByOne(IterableUtil.toArray(expected));
+  }
+
+  /**
+   * Compares the given array of {@link E}s one by one.
+   *
+   * <p>The AssertJ method {@link #containsExactlyInAnyOrderElementsOf} will
+   * output a full list of all non-matching elements which is very difficult to
+   * read if the textual representation of the elements spans several lines.
+   * This method first searches for a matching actual element (using the search
+   * key from {@link #setCompareOneByOneKeyExtractor(Function)}) and then just
+   * compares this one element.
+   *
+   * @param expected an array of {@link E}s; may be null.
+   * @return {@code this} {@link S}; never null.
+   * @see #setCompareOneByOneKeyExtractor(Function)
+   */
+  @SafeVarargs
+  public final S containsExactlyInAnyOrderCompareOneByOne(E ...expected) {
+    return containsExactlyInAnyOrderCompareOneByOneProxy(expected);
+  }
+
+  protected S containsExactlyInAnyOrderCompareOneByOneProxy(E[] expected) {
+    if (actual == null && expected == null) {
+      //noinspection unchecked
+      return (S) this;
+    }
+
+    if (actual != null) {
+      assertExpectedNotNullOfActualIsNotNull(expected);
+    }
+    assert actual != null && expected != null;
+
+    Map<String, E> expectedKeyToElement = Arrays.stream(expected).collect(Collectors.toMap(compareOneByOneKeyExtractor, element -> element));
+    for (E actualElement : actual) {
+      String actualElementKey = compareOneByOneKeyExtractor.apply(actualElement);
+      Assertions.assertThat(expectedKeyToElement.keySet())
+                .as(descriptionText())
+                .contains(actualElementKey);
+
+      toAssert(actualElement, descriptionText())
+              .isEqualTo(expectedKeyToElement.get(actualElementKey));
+    }
+
+    //noinspection unchecked
+    return (S) this;
+  }
+
+  @Override
+  public S withRepresentation(Representation representation) {
+    if (representation instanceof AsmRepresentation) {
+      compareOneByOneKeyExtractor = element -> ((AsmRepresentation<?>) representation).toSimplifiedStringOf(element);
+    }
+
+    return super.withRepresentation(representation);
+  }
+
+  /**
+   * Sets the search key extractor which is gets used by:
+   * <ul>
+   *   <li>{@link #containsExactlyInAnyOrderCompareOneByOneElementsOf(Iterable)};
+   *   <li>and {@link #containsExactlyInAnyOrderCompareOneByOne(Object[])}.
+   * </ul>
+   *
+   * <p>If not called, but the {@link Representation} is an
+   * {@link AsmRepresentation} the returned value of
+   * {@link AsmRepresentation#toSimplifiedStringOf(Object)} will be used.
+   *
+   * @param compareOneByOneKeyExtractor a {@link Function} mapping an element to
+   *                                    a search {@link String} key; never null.
+   * @return {@code this} {@link S}; never null.
+   */
+  public S setCompareOneByOneKeyExtractor(Function<E, String> compareOneByOneKeyExtractor) {
+    this.compareOneByOneKeyExtractor = Objects.requireNonNull(compareOneByOneKeyExtractor);
+
+    //noinspection unchecked
+    return (S) this;
+  }
+
   // -- Private Methods --------------------------------------------------------------------------------------------- //
+
+  private void assertExpectedNotNullOfActualIsNotNull(E[] expected) {
+    Assertions.assertThat(expected)
+              .as(descriptionText())
+              .withFailMessage(String.format("%nExpecting expected value not to be null if actual value is not null"))
+              .isNotNull();
+  }
+
   // -- Inner Type -------------------------------------------------------------------------------------------------- //
 }
---
+++ src/test/java/dev/turingcomplete/asmtestkit/assertion/AsmIterableAssertTest.java
@@ -0,0 +1,132 @@
+package dev.turingcomplete.asmtestkit.assertion;
+
+import dev.turingcomplete.asmtestkit.representation.AbstractAsmRepresentation;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.presentation.StandardRepresentation;
+import org.junit.jupiter.api.Test;
+
+import java.util.Comparator;
+import java.util.List;
+
+class AsmIterableAssertTest {
+  // -- Class Fields ------------------------------------------------------------------------------------------------ //
+  
+  private static final Dummy A = new Dummy("A", "AA");
+  private static final Dummy B = new Dummy("B", "BB");
+  private static final Dummy C = new Dummy("C", "CC");
+  
+  // -- Instance Fields --------------------------------------------------------------------------------------------- //
+  // -- Initialization ---------------------------------------------------------------------------------------------- //
+  // -- Exposed Methods --------------------------------------------------------------------------------------------- //
+
+  @Test
+  void testContainsExactlyInAnyOrderCompareOneByOneElementsOf_missingKeyExtractor() {
+    Assertions.assertThatThrownBy(() -> new DummyAsmIterableAssert(List.of(A, B))
+                      .containsExactlyInAnyOrderCompareOneByOneElementsOf(List.of(A, B)))
+              .isInstanceOf(UnsupportedOperationException.class)
+              .hasMessage("No key extractor for one by one comparison set.");
+  }
+
+  @Test
+  void testContainsExactlyInAnyOrderCompareOneByOneElementsOf_individualKeyExtractor() {
+    new DummyAsmIterableAssert(List.of(A, B))
+            .setCompareOneByOneKeyExtractor(element -> element.key)
+            .containsExactlyInAnyOrderCompareOneByOneElementsOf(List.of(A, B));
+  }
+
+  @Test
+  void testContainsExactlyInAnyOrderCompareOneByOneElementsOf_asmRepresentation() {
+    new DummyAsmIterableAssert(List.of(A, B))
+            .withRepresentation(new AbstractAsmRepresentation<>(Dummy.class) {
+              @Override
+              protected String doToSimplifiedStringOf(Dummy dummy) {
+                return dummy.key;
+              }
+
+              @Override
+              protected String doToStringOf(Dummy dummy) {
+                return dummy.value;
+              }
+            })
+            .containsExactlyInAnyOrderCompareOneByOneElementsOf(List.of(A, B));
+  }
+
+  @Test
+  void testContainsExactlyInAnyOrderCompareOneByOneElementsOf_missingElement() {
+    Assertions.assertThatThrownBy(() -> new DummyAsmIterableAssert(List.of(A, B, C))
+                      .as("Missing element test")
+                      .setCompareOneByOneKeyExtractor(element -> element.key)
+                      .containsExactlyInAnyOrderCompareOneByOneElementsOf(List.of(A, B)))
+              .isInstanceOf(AssertionError.class)
+              .hasMessage("[Missing element test] \n" +
+                          "Expecting KeySet:\n" +
+                          "  [\"A\", \"B\"]\n" +
+                          "to contain:\n" +
+                          "  [\"C\"]\n" +
+                          "but could not find the following element(s):\n" +
+                          "  [\"C\"]\n");
+  }
+
+  @Test
+  void testContainsExactlyInAnyOrderCompareOneByOneElementsOf_invalidElement() {
+    var d1 = new Dummy("D", "DD");
+    var d2 = new Dummy("D", "DDDD");
+
+    Assertions.assertThatThrownBy(() -> new DummyAsmIterableAssert(List.of(A, B, C, d1))
+                      .as("Invalid element test")
+                      .setCompareOneByOneKeyExtractor(element -> element.key)
+                      .containsExactlyInAnyOrderCompareOneByOneElementsOf(List.of(A, B, C, d2)))
+              .isInstanceOf(AssertionError.class)
+              .hasMessage("[Invalid element test] \n" +
+                          "expected: DDDD\n" +
+                          " but was: DD\n" +
+                          "when comparing values using DummyComparator");
+  }
+
+  // -- Private Methods --------------------------------------------------------------------------------------------- //
+  // -- Inner Type -------------------------------------------------------------------------------------------------- //
+
+  private static class Dummy {
+
+    private final String key;
+    private final String value;
+
+    private Dummy(String key, String value) {
+      this.key = key;
+      this.value = value;
+    }
+
+    @Override
+    public String toString() {
+      return value;
+    }
+  }
+
+  // -- Inner Type -------------------------------------------------------------------------------------------------- //
+
+  private static class DummyAsmIterableAssert extends AsmIterableAssert<DummyAsmIterableAssert, Dummy, DummyAsmAssert> {
+
+    public DummyAsmIterableAssert(Iterable<Dummy> actual) {
+      super(actual, DummyAsmIterableAssert.class, DummyAsmAssert::new);
+    }
+  }
+
+  // -- Inner Type -------------------------------------------------------------------------------------------------- //
+
+  private static class DummyAsmAssert extends AsmAssert<DummyAsmAssert, Dummy> {
+
+    protected DummyAsmAssert(Dummy actual) {
+      super("Dummy", actual, DummyAsmAssert.class, StandardRepresentation.STANDARD_REPRESENTATION, new DummyComparator());
+    }
+  }
+
+  // -- Inner Type -------------------------------------------------------------------------------------------------- //
+  
+  private static final class DummyComparator implements Comparator<Dummy> {
+
+    @Override 
+    public int compare(Dummy o1, Dummy o2) {
+      return o1.value.compareTo(o2.value);
+    }
+  }
+}
